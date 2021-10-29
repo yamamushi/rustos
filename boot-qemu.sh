00d59419be981e02feba960f0b6bd5dead8c9737
yamamushi <yamamushi@gmail.com>
2021-10-29 14:50:06 -0600

    rustos init

---
+++ boot-qemu.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+qemu-system-x86_64 -drive format=raw,file=target/x86_64-rust_os/debug/bootimage-rustos.bin
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+#![no_std]
+#![no_main]
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+
+use core::panic::PanicInfo;
+mod vga_buffer;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+
+//// These are for Qemu
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
